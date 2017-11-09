9330259adc465e17deb2bbdb1858c583d5f12203
Ruben Alpredo <alpredo.tampubolon@gmail.com>
2017-11-09 10:17:24 +0700

    Perubahan pada Image/Icon di Bagian Tag Head, dan penghapusan beberapa file Image/Icon

--- application/views/clients/app.php
+++ application/views/clients/app.php
@@ -1,42 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+
+    <!-- Meta Data -->
     <meta charset="utf-8">
     <meta name="robots" content="all,follow">
     <meta name="googlebot" content="index,follow,snippet,archive">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="PPOB, Payment INM, Pay INM">
 
     <title><?php echo $title; ?></title>
 
-    <meta name="keywords" content="">
+    <link rel="shortcut icon" href="<?php echo site_url($basepage . 'img/company-icon/icon.png') ?>" type="image/x-icon" />
+
 
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,500,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- Bootstrap and Font Awesome css -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
     <!-- Css animations  -->
     <link href="<?php echo site_url($basepage . 'css/animate.css'); ?>" rel="stylesheet">
-
-    <!-- Theme stylesheet, if possible do not edit this stylesheet -->
     <link href="<?php echo site_url($basepage . 'css/style.default.css'); ?>" rel="stylesheet" id="theme-stylesheet">
-
-    <!-- Custom stylesheet - for your changes -->
     <link href="<?php echo site_url($basepage . 'css/custom.css'); ?>" rel="stylesheet">
-
-    <link rel="shortcut icon" href="<?php echo site_url($basepage . 'img/favicon.ico') ?>" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="<?php echo site_url($basepage .'img/apple-touch-icon.png'); ?>" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo site_url($basepage .'img/apple-touch-icon-57x57.png'); ?>" />
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo site_url($basepage . 'img/apple-touch-icon-72x72.png'); ?>" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo site_url($basepage . 'img/apple-touch-icon-76x76.png'); ?>" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo site_url($basepage .'img/apple-touch-icon-114x114.png'); ?>" />
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo site_url($basepage . 'img/apple-touch-icon-120x120.png'); ?>" />
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo site_url($basepage .'img/apple-touch-icon-144x144.png'); ?>" />
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo site_url($basepage .'img/apple-touch-icon-152x152.png'); ?>" />
-    <!-- owl carousel css -->
-
     <link href="<?php echo site_url($basepage . 'css/owl.carousel.css'); ?>" rel="stylesheet">
     <link href="<?php echo site_url($basepage . 'css/owl.theme.css'); ?>" rel="stylesheet">
 </head>
@@ -67,9 +52,9 @@
                             </div>
                         </div>
                     </div>
+                    
                     <div class="item">
                         <div class="row">
-
                             <div class="col-sm-7 text-center">
                                 <img class="img-responsive" src="img/template-mac.png" alt="">
                             </div>
