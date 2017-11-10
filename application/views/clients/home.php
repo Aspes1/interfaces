0a75ee9a48eba1bacc9f72a66bb5c4c8b910287d
Ruben Alpredo <alpredo.tampubolon@gmail.com>
2017-11-10 18:45:19 +0700

    1. Penambahan Beberapa Icon Produk PPOB. 2. Perubahan pada file pay-inm.css

---
+++ application/views/clients/home.php
@@ -0,0 +1,114 @@
+<section>
+    <div class="home-carousel">
+        <div class="dark-mask"></div>
+        <div class="container">
+            <div class="homepage owl-carousel">
+                <div class="item">
+                    <div class="row">
+                        <div class="col-sm-5 right">
+                            <h3>Mudah dan Responsive</h3>
+                            <p>Aplikasi kami yang digunakan dalam bertransaksi mudah dan responsi device anda</p>
+                        </div>
+                        <div class="col-sm-7">
+                            <img class="img-responsive" src="<?php echo site_url($basepage . 'img/bg-aplikasi.png'); ?>" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="row">
+                        <div class="col-sm-5 right">
+                            <h3>Keuntungan Besar</h3>
+                            <p>
+                                Keuntugan / FEE yang kami berikan sangat Besar, dari PLN , TELKOM, BPJS Kesehatan dan produk lain yang kami sediakan.
+                            </p>
+                        </div>
+                        <div class="col-sm-7">
+                            <img class="img-responsive" src="<?php echo site_url($basepage . 'img/bg-keuntungan.png'); ?>" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.project owl-slider -->
+        </div>
+    </div>
+
+    <!-- *** HOMEPAGE CAROUSEL END *** -->
+</section>
+
+
+<div id="heading-breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Produk Kami</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="bar background-white no-mb">
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row">
+
+                <div class="col-md-6">
+                    <!-- Pembayaran PLN -->
+                    <div class="media">
+                        <div class="media-left">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/listrik.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Listrik - PLN</h4>
+                            <p>
+                                Pembelian listrik <strong>prabayar (Token)</strong>, pembayaran listrik <strong>pascabayar</strong> dan <strong>non tagihan listrik (NON-TAGLIS) PLN</strong>
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Pembayaran BPJS -->
+                    <div class="media">
+                        <div class="media-left">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/bpjs.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">BPJS</h4>
+                            <p>
+                                Pembayaran asuransi <strong>BPJS Kesehatan</strong> dan <strong>BPJS Ketenagakerjaan</strong>
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Pembayaran Finance -->
+                    <div class="media">
+                        <div class="media-left">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/finance.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Finance dan Kredit</h4>
+                            <p>
+                                Pembayaran <strong>multi finance</strong> dan tagihan <strong>kartu kredit</strong>
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Pembayaran Pulsa -->
+                    <div class="media">
+                        <div class="media-left">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/pulsa.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Pulsa</h4>
+                            <p>
+                                Pembelian pulsa <strong>pasca bayar</strong>
+                                dan <strong>prabayar</strong> untuk semua operator
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
