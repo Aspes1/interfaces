f53417a37d3139dfb064f5446d48cf6e0e6f1e52
Ruben Alpredo <alpredo.tampubolon@gmail.com>
2017-11-11 12:06:39 +0700

    Perubahan pada content, footer dan header. Penambahan Style pada Footer dan Content. Penambahan Text

--- application/views/clients/home.php
+++ application/views/clients/home.php
@@ -36,77 +36,140 @@
     <!-- *** HOMEPAGE CAROUSEL END *** -->
 </section>
 
-
-<div id="heading-breadcrumbs">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h3>Produk Kami</h3>
-            </div>
-        </div>
+<div id="head-cover">
+    <div class="heading">
+        <h2>Daftar Produk PPOB Kami</h2>
     </div>
 </div>
 
 <section class="bar background-white no-mb">
     <div class="container">
         <div class="col-md-12">
-            <div class="row">
-
+            <div class="row"> 
                 <div class="col-md-6">
-                    <!-- Pembayaran PLN -->
-                    <div class="media">
-                        <div class="media-left">
-                            <img src="<?php echo site_url($basepage . 'img/logo-produk/listrik.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                    <!-- Pembayaran Tagihan Listrik PLN -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/listrik.png') ?>" class="media-object img-media img-right">
                         </div>
                         <div class="media-body">
-                            <h4 class="media-heading">Listrik - PLN</h4>
+                            <h4 class="media-heading">Pembayaran Tagihan Listrik PLN</h4>
                             <p>
                                 Pembelian listrik <strong>prabayar (Token)</strong>, pembayaran listrik <strong>pascabayar</strong> dan <strong>non tagihan listrik (NON-TAGLIS) PLN</strong>
                             </p>
                         </div>
                     </div>
 
-                    <!-- Pembayaran BPJS -->
-                    <div class="media">
-                        <div class="media-left">
-                            <img src="<?php echo site_url($basepage . 'img/logo-produk/bpjs.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                    <!-- Pembayaran Tagihan Air PDAM -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/air.png') ?>" class="media-object img-media img-right">
                         </div>
                         <div class="media-body">
-                            <h4 class="media-heading">BPJS</h4>
+                            <h4 class="media-heading">Pembayaran Tagihan Air PDAM</h4>
+                            <p>
+                                Pembayaran tagihan air bersih <strong>PDAM</strong>
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Pembayaran Bulanan BPJS -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/bpjs.png') ?>" class="media-object img-media img-right">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Pembayaran Bulanan BPJS</h4>
                             <p>
                                 Pembayaran asuransi <strong>BPJS Kesehatan</strong> dan <strong>BPJS Ketenagakerjaan</strong>
                             </p>
                         </div>
                     </div>
 
-                    <!-- Pembayaran Finance -->
-                    <div class="media">
-                        <div class="media-left">
-                            <img src="<?php echo site_url($basepage . 'img/logo-produk/finance.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                    <!-- Pembayaran Finance dan Kredit -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/finance.png') ?>" class="media-object img-media img-right">
                         </div>
                         <div class="media-body">
-                            <h4 class="media-heading">Finance dan Kredit</h4>
+                            <h4 class="media-heading">Pembayaran Finance dan Kredit</h4>
                             <p>
                                 Pembayaran <strong>multi finance</strong> dan tagihan <strong>kartu kredit</strong>
                             </p>
                         </div>
                     </div>
 
-                    <!-- Pembayaran Pulsa -->
-                    <div class="media">
-                        <div class="media-left">
-                            <img src="<?php echo site_url($basepage . 'img/logo-produk/pulsa.png') ?>" class="media-object" style="width:60px; margin-right: 20px;">
+                    <!-- Pembelian dan Pengisian Pulsa -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/pulsa.png') ?>" class="media-object img-media img-right">
                         </div>
                         <div class="media-body">
-                            <h4 class="media-heading">Pulsa</h4>
+                            <h4 class="media-heading">Pembelian dan Pengisian Pulsa</h4>
                             <p>
                                 Pembelian pulsa <strong>pasca bayar</strong>
-                                dan <strong>prabayar</strong> untuk semua operator
+                                dan <strong>prabayar</strong> untuk segala jenis operator
                             </p>
                         </div>
                     </div>
                 </div>
 
+
+                <div class="col-md-6">
+                    <!-- Pembayaran Tagihan Langganan TV Kabel -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/televisi.png') ?>" class="media-object img-media img-right">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Pembayaran Tagihan Langganan TV Kabel</h4>
+                            <p>
+                                Pembayaran tagihan pengguna/langganan TV kabel 
+                                <strong>(Indovision, Orange TV, TelkomVision, YesTV, AORA TV)</strong>
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Pembayaran Tiket Pesawat -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/pesawat.png') ?>" class="media-object img-media img-right">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Pembayaran Tiket Pesawat</h4>
+                            <p>
+                                Pemesanan dan pembayaran tiket <strong>pesawat</strong> berbagai maskapai penerbangan
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Reservasi Hotel -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/hotel.png') ?>" class="media-object img-media img-right">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Reservasi Hotel</h4>
+                            <p>
+                                Pembayaran reservasi <strong>hotel</strong> pilihan anda
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Pembayaran Bulanan Asuransi -->
+                    <div class="media media-cover">
+                        <div class="media-left img-cover">
+                            <img src="<?php echo site_url($basepage . 'img/logo-produk/asuransi.png') ?>" class="media-object img-media img-right">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Pembayaran Bulanan Asuransi</h4>
+                            <p>
+                                Pembelian pulsa <strong>pasca bayar</strong>
+                                dan <strong>prabayar</strong> untuk segala jenis operator
+                            </p>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
